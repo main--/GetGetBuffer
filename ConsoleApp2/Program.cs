59a7ee8a0bae23311b33f53f83bb311c7f146a10
main() <main@ehvag.de>
2020-09-13 15:02:38 +0200

    add test project properly

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using GetGetBuffer;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pointers = Class1.DoIt();
+            Debugger.Break();
+        }
+    }
+}
